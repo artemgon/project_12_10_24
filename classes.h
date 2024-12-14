00754015c6ee3ae33bd900b02528175b63abd0d8
artemgon <artemhoncharuk123@gmail.com>
2024-12-14 10:10:54 +0200

    Task 1 is done.

--- classes.h
+++ classes.h
@@ -1,1 +1,89 @@
 #include "libs.h"
+
+template <class T>
+class Node
+{
+public:
+	T data;
+	Node<T>* next;
+	Node(T value) : data(value), next(nullptr) {}
+};
+
+template <class T>
+class LinkedList
+{
+private:
+	Node<T>* head;
+public:
+	LinkedList() : head(nullptr) {}
+	void AddToHead(T value)
+	{
+		Node<T>* newNode = new Node<T>(value);
+		newNode->next = head;
+		head = newNode;
+	}
+	void AddToTail(T value)
+	{
+		Node<T>* newNode = new Node<T>(value);
+		if (head == nullptr)
+		{
+			head = newNode;
+			return;
+		}
+		Node<T>* temp = head;
+		while (temp->next != nullptr)
+		{
+			temp = temp->next;
+		}
+		temp->next = newNode;
+	}
+	void DeleteFromHead()
+	{
+		if (head == nullptr)
+		{
+			cout << "List is empty" << endl;
+			return;
+		}
+		Node<T>* temp = head;
+		head = head->next;
+		delete temp;
+	}
+	void DeleteFromTail()
+	{
+		if (head == nullptr)
+		{
+			cout << "List is empty" << endl;
+			return;
+		}
+		if (head->next == nullptr)
+		{
+			delete head;
+			head = nullptr;
+			return;
+		}
+		Node<T>* temp = head;
+		while (temp->next->next != nullptr)
+		{
+			temp = temp->next;
+		}
+		delete temp->next;
+		temp->next = nullptr;
+	}
+	void DeleteAll()
+	{
+		while (head != nullptr)
+		{
+			DeleteFromHead();
+		}
+	}
+	void Show()
+	{
+		Node<T>* temp = head;
+		while (temp != nullptr)
+		{
+			cout << temp->data << " ";
+			temp = temp->next;
+		}
+		cout << endl;
+	}
+};
--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 
 int main()
 {
+	LinkedList<int> list;
+	for (int i = 0; i < 10; i++)
+	{
+		if (i <= 4)
+			list.AddToHead(5 - i);
+		else if (i > 4)
+			list.AddToTail(i + 1);
+	}
+	cout << "List: " << endl;
+	list.Show();
+	cout << endl;
+	list.DeleteFromTail();
+	list.DeleteAll();
+	cout << "Empty list: " << endl;
+	list.Show();
+	cout << "Void..." << endl;
 	return 0;
 }
